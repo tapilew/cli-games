55eb0c209c5981a8ffa7bca2483dd91d29142a39
Luis Tapia <devlou@protonmail.com>
2023-02-20 14:04:19 -0500

    Set quantity of digits for each operation

--- csharp/MathTrainer/Program.cs
+++ csharp/MathTrainer/Program.cs
@@ -1,8 +1,17 @@
-﻿System.Random random = new System.Random();
+﻿// todo - [x] select quantity of digits for each operand
+// todo - [ ] track time taken to complete each operation and total time
+// todo - [ ] select quantity of operands
+// todo - [ ] select custom ranges (either quantity or digits)
+// todo - [ ] set some defaults
+// todo - [ ] format operations
+// todo - [ ] do unit tests
+
+System.Random random = new System.Random();
 
 int correctAnswers = 0;
 int operations = 0;
-int digits = 0;
+int digitsFirst = 0;
+int digitsSecond = 0;
 
 Console.WriteLine("Operation options:\n" +
     "'addition' (+)\n'subtraction' (-)\n" +
@@ -32,17 +41,21 @@ else
 }
 
 Console.WriteLine($"Great! Let's do {op}");
-Console.Write("Select the amount of digits the operands should have: ");
-digits = System.Convert.ToInt32(Console.ReadLine());
+Console.Write("Select the amount of DIGITS the FIRST operand should have: ");
+digitsFirst = System.Convert.ToInt32(Console.ReadLine());
+Console.Write("Select the amount of DIGITS the SECOND operand should have: ");
+digitsSecond = System.Convert.ToInt32(Console.ReadLine());
 Console.Write("Select the number of operations to perform: ");
 operations = System.Convert.ToInt32(Console.ReadLine());
 
 for (int i = 0; i < operations; i++)
 {
-    int minNum = System.Convert.ToInt32("1" + new string('0', digits - 1));
-    int maxNum = System.Convert.ToInt32("1" + new string('0', digits)) - 1;
-    int operand1 = random.Next(minNum, maxNum);
-    int operand2 = random.Next(minNum, maxNum);
+    int minNumFirst = System.Convert.ToInt32("1" + new string('0', digitsFirst - 1));
+    int maxNumFirst = System.Convert.ToInt32("1" + new string('0', digitsFirst)) - 1;
+    int minNumSecond = System.Convert.ToInt32("1" + new string('0', digitsSecond - 1));
+    int maxNumSecond = System.Convert.ToInt32("1" + new string('0', digitsSecond)) - 1;
+    int operand1 = random.Next(minNumFirst, maxNumFirst);
+    int operand2 = random.Next(minNumSecond, maxNumSecond);
     int result = 0;
     char opSymbol = ' ';
     switch (op)
