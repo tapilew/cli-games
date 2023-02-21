1834878c557475d1ed71bc8013a10358037a518b
Luis Tapia <devlou@protonmail.com>
2023-02-20 20:45:54 -0500

    Track time taken to solve operations

--- csharp/MathTrainer/Program.cs
+++ csharp/MathTrainer/Program.cs
@@ -1,21 +1,25 @@
 ﻿// todo - [x] select quantity of digits for each operand
-// todo - [ ] track time taken to complete each operation and total time
+// todo - [x] track time taken to complete each operation and total time
+// todo - [ ] improve precision in time tracking
 // todo - [ ] select quantity of operands
 // todo - [ ] select custom ranges (either quantity or digits)
 // todo - [ ] set some defaults
 // todo - [ ] format operations
 // todo - [ ] do unit tests
 
+using System.Diagnostics;
 System.Random random = new System.Random();
 
 int correctAnswers = 0;
 int operations = 0;
 int digitsFirst = 0;
 int digitsSecond = 0;
+int totalSeconds = 0;
+int totalMinutes = 0;
 
 Console.WriteLine("Operation options:\n" +
     "'addition' (+)\n'subtraction' (-)\n" +
-    "'multiplication' (*)\n'division' (/)");
+    "'multiplication' (*)\n'division' (/)\n");
 Console.Write("Select your operation type: ");
 var op = Console.ReadLine();
 
@@ -40,13 +44,17 @@ else
     Console.WriteLine("Not a valid option provided... Please try again.");
 }
 
-Console.WriteLine($"Great! Let's do {op}");
+Console.WriteLine($"Great! Let's do {op}\n");
 Console.Write("Select the amount of DIGITS the FIRST operand should have: ");
 digitsFirst = System.Convert.ToInt32(Console.ReadLine());
 Console.Write("Select the amount of DIGITS the SECOND operand should have: ");
 digitsSecond = System.Convert.ToInt32(Console.ReadLine());
 Console.Write("Select the number of operations to perform: ");
 operations = System.Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
 
 for (int i = 0; i < operations; i++)
 {
@@ -79,7 +87,12 @@ for (int i = 0; i < operations; i++)
     }
 
     Console.Write($"{operand1} {opSymbol} {operand2} = ");
+    Stopwatch stopwatchOp = new Stopwatch();
+    stopwatchOp.Start();
     int userAnswer = System.Convert.ToInt32((Console.ReadLine()));
+    stopwatchOp.Stop();
+    int opSeconds = stopwatchOp.Elapsed.Seconds;
+    int opMinutes = stopwatchOp.Elapsed.Minutes;
 
     if (userAnswer == result)
     {
@@ -90,6 +103,12 @@ for (int i = 0; i < operations; i++)
     {
         Console.WriteLine($"Nope... it was {result}");
     }
+    int minutesTakenOp = stopwatchOp.Elapsed.Minutes;
+    Console.WriteLine($"You answered this operation in {opSeconds} seconds ({opMinutes} minutes).\n");
 }
 
+stopwatch.Stop();
+totalSeconds = stopwatch.Elapsed.Seconds;
+totalMinutes = stopwatch.Elapsed.Minutes;
 Console.WriteLine($"Goodbye! your score was {correctAnswers}/{operations}");
+Console.WriteLine($"Total time: {totalSeconds} seconds ({totalMinutes} minutes)");
