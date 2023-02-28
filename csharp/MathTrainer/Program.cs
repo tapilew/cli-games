670419151232f3d89707832d5b3369bf287348a7
Luis Tapia <devlou@protonmail.com>
2023-02-27 21:39:15 -0500

    Implement reusable methods

--- csharp/MathTrainer/Program.cs
+++ csharp/MathTrainer/Program.cs
@@ -1,6 +1,4 @@
-﻿// TODO - [x] new option to convert decimal to binary
-// TODO - [x] new option to convert binary to decimal
-// TODO - [ ] new option to add binary
+﻿// TODO - [ ] new option to add binary
 // TODO - [ ] new option to subtract binary
 // TODO - [ ] determine value of binary with 1 in each place
 // TODO - [ ] determine how many 1s would be enough to represent n number
@@ -15,146 +13,236 @@ using System.Diagnostics;
 using System.Text;
 System.Random random = new System.Random();
 
-var mode = "";
-Console.WriteLine("Welcome to the math trainer!\n\nSelect what you want to do...");
-Console.WriteLine("1) arithmetic\n2) binary");
-Console.Write("Choose: ");
-mode = Console.ReadLine();
-if (mode == "1" || mode == "arithmetic")
+static int DecimalToBinary(int decimalValue)
 {
-    int correctAnswers = 0;
-    int operations = 0;
-    int operands = 0;
-    int totalSeconds = 0;
-    int totalMinutes = 0;
-
-    Console.WriteLine("\nOperation options:\n" +
-        "'addition' (+)\n'subtraction' (-)\n" +
-        "'multiplication' (*)\n'division' (/)");
-    Console.Write("Select your operation type: ");
-
-    var op = Console.ReadLine();
-
-    if (op == "addition" || op == "add" || op == "+")
+    string binaryText = "";
+    int quotient = decimalValue;
+    while (quotient > 1)
     {
-        op = "addition";
+        int remainder = quotient % 2;
+        quotient /= 2;
+        binaryText = remainder + binaryText;
     }
-    else if (op == "subtraction" || op == "subtract" || op == "sub" || op == "-")
+    binaryText = 1 + binaryText;
+    return Convert.ToInt32(binaryText);
+}
+
+static string IdentifyOperationType(string operation)
+{
+
+    if (operation == "addition" ||
+        operation == "add" ||
+        operation == "+")
     {
-        op = "subtraction";
+        return "addition";
     }
-    else if (op == "multiplication" || op == "multiply" || op == "mlp" || op == "*")
+    else if (operation == "subtraction" ||
+        operation == "subtract" ||
+        operation == "sub" ||
+        operation == "-")
     {
-        op = "multiplication";
+        return "subtraction";
     }
-    else if (op == "division" || op == "divide" || op == "div" || op == "/")
+    else if (operation == "multiplication" ||
+        operation == "multiply" ||
+        operation == "mlp" ||
+        operation == "*")
     {
-        op = "division";
+        return "multiplication";
+    }
+    else if (operation == "division" ||
+        operation == "divide" ||
+        operation == "div" ||
+        operation == "/")
+    {
+        return "division";
     }
     else
     {
-        Console.WriteLine("Not a valid option provided... Please try again.");
+        return "nope...";
     }
 
-    Console.WriteLine($"Great! Let's do {op}\n");
-    Console.Write("Select the amount of OPERANDS of each operation: ");
+}
 
-    operands = Convert.ToInt32(Console.ReadLine());
+static string ReportTotalScore(
+    int correctAnswers,
+    int totalQuestions,
+    int totalSeconds,
+    int totalMinutes)
+{
+    return ($"Goodbye! your score was {correctAnswers}/{totalQuestions}\n") +
+        ($"Total time: {totalSeconds} seconds ({totalMinutes} minutes)");
+}
+
+static string WelcomeMessage(string state)
+{
+    switch (state)
+    {
+        case "home":
+            return "Welcome to the math trainer!\n\n" +
+                "Select what you want to do...\n" +
+                "1) arithmetic\n2) binary\n" +
+                "Choose: ";
+        case "arithmetic":
+            return "\nOperation options:\n" +
+                "'addition' (+)\n'subtraction' (-)\n" +
+                "'multiplication' (*)\n'division' (/)\n" +
+                "Select your operation type: ";
+        case "binary":
+            return "\n1) decimal to binary\n2) binary to decimal\n" +
+                "Choose: ";
+        default:
+            return "nope...";
+    }
+}
+
+static string ReportQuestionResult(
+    int userAnswer,
+    int correctAnswer,
+    int seconds,
+    int minutes)
+{
+    string message = "";
+    bool answeredRight = userAnswer == correctAnswer;
+    message += answeredRight ? "That's correct!" : $"Nope... it was {correctAnswer}";
+    message += $"\nYou answered in {seconds} seconds ({minutes} minutes).\n";
+    return message;
+}
+
+static int SolveOperation(string operationType, int[] operands)
+{
+    int result = operands[0];
+    for (int i = 1; i < operands.Length; i++)
+    {
+        switch (operationType)
+        {
+            case "addition":
+                result += operands[i];
+                break;
+            case "subtraction":
+                result -= operands[i];
+                break;
+            case "multiplication":
+                result *= operands[i];
+                break;
+            case "division":
+                result /= operands[i];
+                break;
+        }
+    }
+    return result;
+}
+
+static string PromptOperation(string operationType, int[] operands)
+{
+    StringBuilder sb = new StringBuilder();
+    sb.Append(operands[0]);
+    char opSymbol = ' ';
+    switch (operationType)
+    {
+        case "addition":
+            opSymbol = '+';
+            break;
+        case "subtraction":
+            opSymbol = '-';
+            break;
+        case "multiplication":
+            opSymbol = '*';
+            break;
+        case "division":
+            opSymbol = '/';
+            break;
+    }
+    for (int j = 1; j < operands.Length; j++)
+    {
+        sb.Append($" {opSymbol} {operands[j]}");
+    }
+    sb.Append(" = ");
+    return sb.ToString();
+}
+
+static int PromptForNumber(string prompt)
+{
+    Console.Write(prompt);
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+static string PromptForString(string prompt)
+{
+    Console.Write(prompt);
+    string answer = Console.ReadLine();
+    return answer;
+}
+
+static int GetMinimumNumberWithNDigits(int digits)
+{
+    return Convert.ToInt32("1" + new string('0', digits - 1));
+}
+
+static int GetMaximumNumberWithNDigits(int digits)
+{
+    return Convert.ToInt32("1" + new string('0', digits)) - 1;
+}
+
+var mode = PromptForString(WelcomeMessage("home"));
+
+if (mode == "1" || mode == "arithmetic")
+{
+    int correctAnswers = 0;
+    var operationType = PromptForString(WelcomeMessage("arithmetic"));
+    operationType = IdentifyOperationType(operationType);
+    int operands = PromptForNumber($"\nGreat! Let's do {operationType}\n" +
+        "Select the amount of OPERANDS of each operation: ");
     int[] digitsPerOperand = new int[operands];
-
     for (int i = 0; i < operands; i++)
     {
         int operandNumber = i + 1;
-        Console.Write($"How many DIGITS should operand {operandNumber} have: ");
-        digitsPerOperand[i] = Convert.ToInt32(Console.ReadLine());
+        digitsPerOperand[i] = PromptForNumber(
+            $"How many DIGITS should operand {operandNumber} have: ");
     }
-
-    Console.Write("\nSelect the number of operations to perform: ");
-    operations = Convert.ToInt32(Console.ReadLine());
+    int operations = PromptForNumber("\n Select the number of operations to perform: ");
     Console.WriteLine();
-
     Stopwatch stopwatch = new Stopwatch();
     stopwatch.Start();
-
     for (int i = 0; i < operations; i++)
     {
         int[] operandValues = new int[operands];
         for (int j = 0; j < operands; j++)
         {
-            int min = Convert.ToInt32("1" + new string('0', digitsPerOperand[j] - 1));
-            int max = Convert.ToInt32("1" + new string('0', digitsPerOperand[j])) - 1;
-            int value = random.Next(min, max);
+            int minimum = GetMinimumNumberWithNDigits(digitsPerOperand[j]);
+            int maximum = GetMaximumNumberWithNDigits(digitsPerOperand[j]);
+            int value = random.Next(minimum, maximum);
             operandValues[j] = value;
         }
-        char opSymbol = ' ';
-        int result = operandValues[0];
-        StringBuilder sb = new StringBuilder();
-        sb.Append(operandValues[0]);
-        for (int j = 1; j < operands; j++)
-        {
-            switch (op)
-            {
-                case "addition":
-                    opSymbol = '+';
-                    result += operandValues[j];
-                    break;
-                case "subtraction":
-                    opSymbol = '-';
-                    result -= operandValues[j];
-                    break;
-                case "multiplication":
-                    opSymbol = '*';
-                    result *= operandValues[j];
-                    break;
-                case "division":
-                    opSymbol = '/';
-                    result /= operandValues[j];
-                    break;
-            }
-            sb.Append($" {opSymbol} {operandValues[j]}");
-        }
-
-        Console.Write($"{sb.ToString()} = ");
         Stopwatch stopwatchOp = new Stopwatch();
         stopwatchOp.Start();
-        int userAnswer = Convert.ToInt32((Console.ReadLine()));
+        int userAnswer = PromptForNumber(PromptOperation(operationType, operandValues));
+        int result = SolveOperation(operationType, operandValues);
         stopwatchOp.Stop();
-        if (userAnswer == result)
-        {
-            correctAnswers++;
-            Console.WriteLine("That's correct!");
-        }
-        else
-        {
-            Console.WriteLine($"Nope... it was {result}");
-        }
-        int opMinutes = stopwatchOp.Elapsed.Minutes;
-        int opSeconds = stopwatchOp.Elapsed.Seconds;
-        Console.WriteLine($"You answered this operation in {opSeconds}" +
-            $"seconds ({opMinutes} minutes).\n");
+        if (userAnswer == result) correctAnswers++;
+        Console.WriteLine(ReportQuestionResult(
+            userAnswer,
+            result,
+            stopwatchOp.Elapsed.Seconds,
+            stopwatchOp.Elapsed.Minutes));
     }
 
     stopwatch.Stop();
-    totalSeconds = stopwatch.Elapsed.Seconds;
-    totalMinutes = stopwatch.Elapsed.Minutes;
-    Console.WriteLine($"Goodbye! your score was {correctAnswers}/{operations}");
-    Console.WriteLine($"Total time: {totalSeconds} seconds ({totalMinutes} minutes)");
+    Console.WriteLine(ReportTotalScore(
+        correctAnswers,
+        operations,
+        stopwatch.Elapsed.Seconds,
+        stopwatch.Elapsed.Minutes));
 }
 else if (mode == "2" || mode == "binary")
 {
-    Console.WriteLine("\n1) decimal to binary\n2) binary to decimal");
-    Console.Write("Choose: ");
-    var binaryMode = Console.ReadLine();
-    Console.WriteLine("\nWrite a range of numbers to convert from or to");
-    int minimum = 0;
-    int maximum = 0;
-    int conversions = 0;
-    Console.Write("Minimum: ");
-    minimum = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Maximum: ");
-    maximum = Convert.ToInt32(Console.ReadLine());
-    Console.Write("How many times you want to convert?: ");
-    conversions = Convert.ToInt32(Console.ReadLine());
+    var binaryMode = PromptForString(WelcomeMessage("binary"));
+    int minimum = PromptForNumber(
+        "Write a range of numbers to convert from or to\n" +
+        "Minimum: ");
+    int maximum = PromptForNumber("Maximum: ");
+    int conversions = PromptForNumber("How many times you want to convert?: ");
     Console.WriteLine();
     int[] decimals = new int[conversions];
     int[] binaries = new int[conversions];
@@ -162,88 +250,61 @@ else if (mode == "2" || mode == "binary")
     {
         int decimalValue = random.Next(minimum, maximum + 1);
         decimals[i] = decimalValue;
-        string binaryText = "";
-        int quotient = decimalValue;
-        while (quotient > 1)
-        {
-            int remainder = quotient % 2;
-            quotient /= 2;
-            binaryText = remainder + binaryText;
-        }
-        binaryText = 1 + binaryText;
-        binaries[i] = Convert.ToInt32(binaryText);
+        binaries[i] = DecimalToBinary(decimalValue);
     }
     if (binaryMode == "1" || binaryMode == "decimal to binary")
     {
         int correctAnswers = 0;
-        int totalSeconds = 0;
-        int totalMinutes = 0;
         Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
         for (int i = 0; i < conversions; i++)
         {
             int decimalValue = decimals[i];
             int binaryValue = binaries[i];
-            Console.Write($"Convert {decimalValue} to binary: ");
             Stopwatch stopwatchOp = new Stopwatch();
             stopwatchOp.Start();
-            int userAnswer = Convert.ToInt32(Console.ReadLine());
+            int userAnswer = PromptForNumber($"Convert {decimalValue} to binary: ");
             stopwatchOp.Stop();
-            int secondsToConvert = stopwatchOp.Elapsed.Seconds;
-            int minutesToConvert = stopwatchOp.Elapsed.Minutes;
-            if (userAnswer == binaryValue)
-            {
-                correctAnswers++;
-                Console.WriteLine("That's correct!");
-            }
-            else
-            {
-                Console.WriteLine($"Nope... it was {binaryValue}");
-            }
-            Console.WriteLine($"You converted this number in " +
-                $"{secondsToConvert} seconds ({minutesToConvert} minutes).\n");
+            if (userAnswer == binaryValue) correctAnswers++;
+            Console.WriteLine(ReportQuestionResult(
+                userAnswer,
+                binaryValue,
+                stopwatchOp.Elapsed.Seconds,
+                stopwatchOp.Elapsed.Minutes));
         }
         stopwatch.Stop();
-        totalSeconds = stopwatch.Elapsed.Seconds;
-        totalMinutes = stopwatch.Elapsed.Minutes;
-        Console.WriteLine($"Goodbye! your score was {correctAnswers}/{conversions}");
-        Console.WriteLine($"Total time: {totalSeconds} seconds ({totalMinutes} minutes)");
+        Console.WriteLine(ReportTotalScore(
+            correctAnswers,
+            conversions,
+            stopwatch.Elapsed.Seconds,
+            stopwatch.Elapsed.Minutes));
     }
     else if (binaryMode == "2" || binaryMode == "binary to decimal")
     {
         int correctAnswers = 0;
-        int totalSeconds = 0;
-        int totalMinutes = 0;
         Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
         for (int i = 0; i < conversions; i++)
         {
             int decimalValue = decimals[i];
             int binaryValue = binaries[i];
-            Console.Write($"Convert {binaryValue} to decimal: ");
             Stopwatch stopwatchOp = new Stopwatch();
             stopwatchOp.Start();
-            int userAnswer = Convert.ToInt32(Console.ReadLine());
+            int userAnswer = PromptForNumber($"Convert {binaryValue} to decimal: ");
             stopwatchOp.Stop();
-            int secondsToConvert = stopwatchOp.Elapsed.Seconds;
-            int minutesToConvert = stopwatchOp.Elapsed.Minutes;
-            if (userAnswer == decimalValue)
-            {
-                correctAnswers++;
-                Console.WriteLine("That's correct!");
-            }
-            else
-            {
-                Console.WriteLine($"Nope... it was {decimalValue}");
-            }
-            Console.WriteLine($"You converted this number in " +
-                $"{secondsToConvert} seconds ({minutesToConvert} minutes).\n");
+            if (userAnswer == decimalValue) correctAnswers++;
+            Console.WriteLine(ReportQuestionResult(
+                userAnswer,
+                decimalValue,
+                stopwatchOp.Elapsed.Seconds,
+                stopwatchOp.Elapsed.Minutes));
         }
         stopwatch.Stop();
-        totalSeconds = stopwatch.Elapsed.Seconds;
-        totalMinutes = stopwatch.Elapsed.Minutes;
-        Console.WriteLine($"Goodbye! your score was {correctAnswers}/{conversions}");
-        Console.WriteLine($"Total time: {totalSeconds} seconds ({totalMinutes} minutes)");
+        Console.WriteLine(ReportTotalScore(
+            correctAnswers,
+            conversions,
+            stopwatch.Elapsed.Seconds,
+            stopwatch.Elapsed.Minutes));
     }
     else
     {
