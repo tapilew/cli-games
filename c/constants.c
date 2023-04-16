39df14eee3ee6ae13674fbee09d21eea34134390
Luis Tapia <devlou@protonmail.com>
2023-04-16 18:41:14 -0500

    feat: use a preprocessor define constant

--- c/constants.c
+++ c/constants.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+
+const char NEW_LINE = '\n';
+const int HOTDOG_COST = 100ul; // suffixes for unsigned and long
+const float TOTAL_BILL = 100.07;
+
+#define PIZZA_COST 1.5
+
 int main()
 {
-    const char newLine = '\n';
+    float pizzaPrice;
+    float numberOfSlices = 3;
+    pizzaPrice = PIZZA_COST * numberOfSlices;
+    printf("Total bill: %f", pizzaPrice);
+    printf("%c", NEW_LINE);
+
     // C has not string data type
-    printf("it's human to fail%c", newLine); // a string literal
-    const int hotDogPrice = 100ul; // suffixes for unsigned and long
-    printf("hotDogPrice: %d%c", hotDogPrice, newLine);
-    const float totalBill = 100.07;
-    printf("totalBill: %f%c", totalBill, newLine);
+    printf("it's human to fail%c", NEW_LINE); // a string literal
+    printf("HOTDOG_COST: %d%c", HOTDOG_COST, NEW_LINE);
+    printf("TOTAL_BILL: %f%c", TOTAL_BILL, NEW_LINE);
 }
